ddb4b53543a0715f5e0b63a16565821c543765a0
Gabriel Batista Barbosa <gabriel.batista@saude.se.gov.br>
2026-09-10 11:34:08 -0300

    feat(workout-logs): add entity and repository

--- src/main/java/com/gbatistadev/gymbro/workout_logs/WorkoutLog.java
+++ src/main/java/com/gbatistadev/gymbro/workout_logs/WorkoutLog.java
@@ -1,11 +1,36 @@
 package com.gbatistadev.gymbro.workout_logs;
 
-@lombok.Getter
-@lombok.Setter@jakarta.persistence.Entity
-@jakarta.persistence.Table(name = "workout_log")
-public class WorkoutLog extends com.gbatistadev.gymbro.common.BaseEntity {
+import com.gbatistadev.gymbro.common.BaseEntity;
+import com.gbatistadev.gymbro.exercise.Exercise;
+import com.gbatistadev.gymbro.workout_session.WorkoutSession;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "workout_log")
+public class WorkoutLog extends BaseEntity {
+    @Column(name = "set_number", nullable = false)
+    private Integer setNumber;
 
+    @Column(nullable = false, precision = 1)
+    private Double weight;
 
+    @Column(nullable = false)
+    private Integer reps;
 
+    @Column(name = "is_pr", nullable = false)
+    private Boolean isPr;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "workout_session_id", nullable = false)
+    private WorkoutSession workoutSession;
 }
--- src/main/java/com/gbatistadev/gymbro/workout_logs/WorkoutLogRepository.java
+++ src/main/java/com/gbatistadev/gymbro/workout_logs/WorkoutLogRepository.java
@@ -1,4 +1,9 @@
 package com.gbatistadev.gymbro.workout_logs;
 
-public interface WorkoutLogRepository extends org.springframework.data.jpa.repository.JpaRepository<com.gbatistadev.gymbro.workout_logs.WorkoutLog, java.util.UUID> ,org.springframework.data.jpa.repository.JpaSpecificationExecutor<com.gbatistadev.gymbro.workout_logs.WorkoutLog> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface WorkoutLogRepository extends JpaRepository<WorkoutLog, UUID>, JpaSpecificationExecutor<WorkoutLog> {
 }
