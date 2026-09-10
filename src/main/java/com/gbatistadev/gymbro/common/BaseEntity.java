21d1c40de155e9914de5b8dc02a8e948082b549c
Gabriel Batista Barbosa <gabriel.batista@saude.se.gov.br>
2026-09-10 11:34:15 -0300

    refactor(common): update BaseEntity

--- src/main/java/com/gbatistadev/gymbro/common/BaseEntity.java
+++ src/main/java/com/gbatistadev/gymbro/common/BaseEntity.java
@@ -6,25 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false)
+    @JdbcTypeCode(SqlTypes.TIMESTAMP_WITH_TIMEZONE)
     private Instant createdAt;
 
-    @LastModifiedDate
-    @Column(nullable = false)
+    @Column(name = "updated_at")
+    @JdbcTypeCode(SqlTypes.TIMESTAMP_WITH_TIMEZONE)
     private Instant updatedAt;
 }
