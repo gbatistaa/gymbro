09a78cc63b6734dbd9ff0f83d02a1c12036bb099
Gabriel Batista Barbosa <gabriel.batista@saude.se.gov.br>
2026-09-10 14:04:17 -0300

    chore(config): bind spring.jwt properties with metadata support

---
+++ src/main/java/com/gbatistadev/gymbro/config/jwt/JwtProperties.java
@@ -0,0 +1,10 @@
+package com.gbatistadev.gymbro.config.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.jwt")
+public record JwtProperties(
+        String secret,
+        Long expirationMs
+) {
+}
