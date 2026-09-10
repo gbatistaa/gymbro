1c76e624fc40861313eccef90e3f51cc37a527e2
gbatistaa <gbatistadev@gmail.com>
2026-09-09 22:22:55 -0300

    feat(common): add base entity with auditing and enable JPA auditing

---
+++ src/main/java/com/gbatistadev/gymbro/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.gbatistadev.gymbro.common;
+
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
